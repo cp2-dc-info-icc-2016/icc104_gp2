020cab29ad83079677ca347997127381afc8ea53
mcwytin <wyctorhector@gmail.com>
2017-02-16 10:26:48 -0300

    correção no design

---
+++ nos conheça/nosconheca.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+
+func1= {
+  nome= "Ashley",
+  cargo= "Diretora Comercial",
+  email= "ashleymesquita@zara.com",
+  immagem:"ash.jpeg",
+},
+
+
+func2= {
+  nome= "Rizia",
+  cargo= "Produtora de Casting",
+  email= "rizia.custodio@zara.com",
+  imagem: "izzi.jpeg",
+},
+
+func3= {
+  nome= "Bianca",
+  cargo= "Gerente Administrativa",
+  email= "biancacm@zara.com",
+  imagem: "bianca.jpeg",
+},
+
+func4= {
+  nome= "Alana",
+  cargo= "Gerente Comercial",
+  email= "alanapn@zara.com",
+  imagem:"lana.jpeg",
+
+},
